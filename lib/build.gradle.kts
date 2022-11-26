b86ba9f8958d77a85918b506c34941f19a94403a
guohao <guohaoice@gmail.com>
2022-11-26 17:44:21 +0800

    remove template files

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -28,15 +28,9 @@ dependencies {
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:30.1.1-jre")
-
     // https://mvnrepository.com/artifact/io.netty/netty-all
-    implementation("io.netty:netty-all:4.1.85.Final")
+    api("io.netty:netty-all:4.1.85.Final")
 
-
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api("org.apache.commons:commons-math3:3.6.1")
 }
 
 testing {
