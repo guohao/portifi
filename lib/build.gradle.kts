ac5d449d6b6640c16269dfd9eceea0dad694c3c7
guohao <guohaoice@gmail.com>
2022-12-18 06:33:42 +0800

    start new dev epoch to 0.0.2

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -55,7 +55,7 @@ publishing {
         register<MavenPublication>("gpr") {
             from(components["java"])
             groupId = "io.github.gh"
-            version = "0.0.1"
+            version = "0.0.2-SNAPSHOT"
             artifactId = "portifi"
         }
     }
